c9400f45388b1b0e0ca401d09066861f0d9ca31e
DeeCaaD <perttu.kangas@hotmail.fi>
2024-10-16 12:36:46 +0300

    3 3

--- deploy.sh
+++ deploy.sh
@@ -8,50 +8,42 @@ fi
 RELATIVE_PATH=$1
 VERSION=${2:-1}
 
-if [ -d $RELATIVE_PATH/applications ]; then
-  cd $RELATIVE_PATH/applications
-  for DIR in */ ; do
-    if [ -d "$DIR" ]; then
-      SANITIZED_TAG=$(echo $RELATIVE_PATH$DIR | sed 's|^\./||' | tr '/' '-' | sed 's/-$//')
-      docker build $DIR -t $SANITIZED_TAG-$VERSION
-      docker tag $SANITIZED_TAG-$VERSION deecaad/devopsk8s:$SANITIZED_TAG-$VERSION
-      docker push deecaad/devopsk8s:$SANITIZED_TAG-$VERSION
+cd $RELATIVE_PATH/applications
+for DIR in */ ; do
+  if [ -d "$DIR" ]; then
+    SANITIZED_TAG=$(echo $RELATIVE_PATH$DIR | sed 's|^\./||' | tr '/' '-' | sed 's/-$//')
+    docker build --tag "deecaad/devopsk8s:$SANITIZED_TAG-$VERSION" $DIR
+    docker push deecaad/devopsk8s:$SANITIZED_TAG-$VERSION
+    
+    #find ../k8s/ -type f -name "*.yaml" -exec sed -i "s|\(image: deecaad/devopsk8s:$SANITIZED_TAG\).*|\1-$VERSION|g" {} +
+  fi
+done
+cd ..
 
-      find ../k8s/ -type f -name "*.yaml" -exec sed -i "s|\(image: deecaad/devopsk8s:$SANITIZED_TAG\).*|\1-$VERSION|g" {} +
-    fi
-  done
-  cd ..
-else
-  cd $RELATIVE_PATH
-  SANITIZED_TAG=$(echo $RELATIVE_PATH | sed 's|^\./||' | tr '/' '-' | sed 's/-$//')
-  
-  docker build . -t $SANITIZED_TAG-$VERSION
-  docker tag $SANITIZED_TAG-$VERSION deecaad/devopsk8s:$SANITIZED_TAG-$VERSION
-  docker push deecaad/devopsk8s:$SANITIZED_TAG-$VERSION
-
-  find . -type f -name "*.yaml" -exec sed -i "s|\(image: deecaad/devopsk8s:$SANITIZED_TAG\).*|\1-$VERSION|g" {} +
+if [ -d ./k8s/ ]; then
+  kubectl apply -k ./k8s/
 fi
 
-if [ -d ./k8s/namespaces ]; then
-  kubectl apply -f ./k8s/namespaces/
-fi
-
-if [ -f ./k8s/secrets/secret.yaml ]; then
-  kubectl apply -f ./k8s/secrets/secret.yaml
-fi
-
-if [ -d ./k8s/crons ]; then
-  kubectl apply -f ./k8s/crons/
-fi
-
-if [ -d ./k8s/volumes ]; then
-  kubectl apply -f ./k8s/volumes/
-fi
-
-if [ -d ./k8s/manifests ]; then
-  kubectl apply -f ./k8s/manifests/
-fi
-
-if [ -d ./manifests ]; then
-  kubectl apply -f ./manifests/
-fi
+#if [ -d ./k8s/namespaces ]; then
+#  kubectl apply -f ./k8s/namespaces/
+#fi
+#
+#if [ -f ./k8s/secrets/secret.yaml ]; then
+#  kubectl apply -f ./k8s/secrets/secret.yaml
+#fi
+#
+#if [ -d ./k8s/crons ]; then
+#  kubectl apply -f ./k8s/crons/
+#fi
+#
+#if [ -d ./k8s/volumes ]; then
+#  kubectl apply -f ./k8s/volumes/
+#fi
+#
+#if [ -d ./k8s/manifests ]; then
+#  kubectl apply -f ./k8s/manifests/
+#fi
+#
+#if [ -d ./manifests ]; then
+#  kubectl apply -f ./manifests/
+#fi
