3b82ee408bc85024b81f307dedd789bb35ea06fd
DeeCaaD <perttu.kangas@hotmail.fi>
2024-10-16 16:01:49 +0300

    Strip duplicates

--- part3/3-5/applications/todo-frontend/client/src/App.jsx
+++ part3/3-5/applications/todo-frontend/client/src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
 
   return (
     <>
-      <h1>Hello from a static page</h1>
+      <h1>Hello from a page</h1>
       <img
         src='/images/image.jpg'
         alt='Image description'
