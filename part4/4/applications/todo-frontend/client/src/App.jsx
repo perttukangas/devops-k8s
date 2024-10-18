bac85763a7c8af673d534fd0b7f88c73caf4259b
DeeCaaD <perttu.kangas@hotmail.fi>
2024-10-18 21:16:08 +0300

    4 3-4

---
+++ part4/4/applications/todo-frontend/client/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+
+const App = () => {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch('/todos')
+        const data = await response.json()
+        setTodos(data)
+      } catch (error) {
+        console.error('Error fetching todos:', error)
+      }
+    }
+
+    fetchTodos()
+  }, [])
+
+  const handleCreateTodo = async () => {
+    if (newTodo.trim() === '') return
+
+    try {
+      const response = await fetch('/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ todo: newTodo }),
+      })
+
+      if (response.ok) {
+        const createdTodo = await response.json()
+        setTodos([...todos, createdTodo])
+        setNewTodo('')
+      } else {
+        console.error('Error creating todo:', response.statusText)
+      }
+    } catch (error) {
+      console.error('Error creating todo:', error)
+    }
+  }
+
+  return (
+    <>
+      <h1>Hello from a page</h1>
+      <img
+        src='/images/image.jpg'
+        alt='Image description'
+        style={{ maxWidth: '512px', maxHeight: '512px' }}
+      />
+
+      <h2>Todo List</h2>
+      <input
+        type='text'
+        id='todoInput'
+        maxLength='140'
+        placeholder='Enter your todo'
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button id='createTodo' onClick={handleCreateTodo}>
+        Create
+      </button>
+
+      <ul id='todoList'>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo.todo}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
