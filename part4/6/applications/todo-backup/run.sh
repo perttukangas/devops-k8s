d3592aab21210936ec126a3563952e055e5fbac1
DeeCaaD <perttu.kangas@hotmail.fi>
2024-10-19 16:42:43 +0300

    4 6

---
+++ part4/6/applications/todo-backup/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ -z "$POSTGRES_DB" || -z "$POSTGRES_USER" || -z "$POSTGRES_HOST" || -z "$POSTGRES_PASSWORD" || -z "$POSTGRES_PORT" ]]; then # || -z "$GCLOUD_TOKEN" ]]; then
+  echo "One or more required environment variables are not set."
+  echo "Please set POSTGRES_DB, POSTGRES_USER, POSTGRES_HOST, POSTGRES_PASSWORD, and POSTGRES_PORT" #, and GCLOUD_TOKEN."
+  exit 1
+fi
+
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+export PGPASSWORD="$POSTGRES_PASSWORD"
+pg_dump -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > "./todo_backup_$TIMESTAMP.sql"
+
+if [[ $? -eq 0 ]]; then
+  echo "Database backup successful."
+else
+  echo "Database backup failed."
+  exit 1
+fi
+
+OBJECT_LOCATION="./todo_backup_$TIMESTAMP.sql"
+OBJECT_CONTENT_TYPE="application/sql"
+OBJECT_NAME="todo_backup_$TIMESTAMP.sql"
+BUCKET_NAME="dwk-gke-bucket"
+
+#curl -X POST --data-binary @"$OBJECT_LOCATION" \
+#  -H "Authorization: Bearer $GCLOUD_TOKEN" \
+#  -H "Content-Type: $OBJECT_CONTENT_TYPE" \
+#  "https://storage.googleapis.com/upload/storage/v1/b/$BUCKET_NAME/o?uploadType=media&name=$OBJECT_NAME"
---
+++ part4/6/applications/todo-broadcaster/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func connectNats() *nats.Conn {
+	natsURL := os.Getenv("NATS_URL")
+	for {
+		nc, err := nats.Connect(natsURL)
+		if err == nil {
+			fmt.Println("Connected to NATS successfully.")
+			return nc
+		}
+		fmt.Println("Failed to connect to NATS. Retrying in 5 seconds...")
+		time.Sleep(5 * time.Second)
+	}
+}
+
+var (
+	nc *nats.Conn
+)
+
+func main() {
+	nc = connectNats()
+	defer nc.Close()
+
+	_, err := nc.QueueSubscribe("todo-updates", "todo-group", func(m *nats.Msg) {
+		webhookURL := os.Getenv("DISCORD_WEBHOOK")
+		if webhookURL == "" {
+			fmt.Println("DISCORD_WEBHOOK environment variable is not set.")
+			return
+		}
+
+		message := fmt.Sprintf("Received a todo update: %s", string(m.Data))
+		payload := fmt.Sprintf(`{"content": "%s"}`, message)
+
+		req, err := http.NewRequest("POST", webhookURL, strings.NewReader(payload))
+		if err != nil {
+			fmt.Printf("Failed to create request: %v\n", err)
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Failed to send request: %v\n", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Failed to send message to Discord, status code: %d\n", resp.StatusCode)
+		} else {
+			fmt.Printf("Message sent to Discord successfully: %s\n", message)
+		}
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to subscribe to todo-updates: %v\n", err)
+		return
+	}
+
+	select {}
+}
